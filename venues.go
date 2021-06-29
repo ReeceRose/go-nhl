d32e1dc12d21400a3aa9f8b6fc9f8cac0b08aa60
Reece Rose <reecerose18@gmail.com>
2021-06-29 18:48:00 -0400

    Updated venues API wrapper

--- venues.go
+++ venues.go
@@ -2,16 +2,26 @@ package gonhl
 
 import (
 	"encoding/json"
+	"strconv"
 )
 
-func (c *Client) GetVenues() ([]Venue, int, error) {
+func (c *Client) getVenues(appendedURL string) ([]Venue, int, error) {
 	var response venueResponse
-	data, statusCode, err := c.get(STATS_BASE_URL + "/venues")
+	data, statusCode, err := c.get(STATS_BASE_URL + "/venues/" + appendedURL)
 	if err != nil {
 		return response.Venues, statusCode, err
 	}
 	json.Unmarshal(data, &response)
 	return response.Venues, statusCode, nil
+}
+
+func (c *Client) GetVenues() ([]Venue, int, error) {
+	return c.getVenues("")
+}
+
+func (c *Client) GetVenueById(id int) (Venue, int, error) {
+	venues, statusCode, err := c.getVenues(strconv.Itoa(id))
+	return venues[0], statusCode, err
 }
 
 type venueResponse struct {
