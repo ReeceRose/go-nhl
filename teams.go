d8b6355c8d9ff399c10783f0804e29cbb7376428
Reece Rose <reecerose18@gmail.com>
2021-06-30 17:24:49 -0400

    Added GetTeamPlayers to teams

--- teams.go
+++ teams.go
@@ -27,6 +27,16 @@ func (c *Client) GetTeamById(id int) (Team, int, error) {
 	return response.Teams[0], statusCode, nil
 }
 
+func (c *Client) GetTeamPlayersById(id int) (TeamPlayers, int, error) {
+	var response TeamPlayers
+	data, statusCode, err := c.get(RECORDS_BASE_URL + "/player/byTeam/" + strconv.Itoa(id))
+	if err != nil {
+		return response, statusCode, err
+	}
+	json.Unmarshal(data, &response)
+	return response, statusCode, nil
+}
+
 func (c *Client) GetTeamStatsById(id int) ([]Stat, int, error) {
 	var response teamStatsResponse
 	data, statusCode, err := c.get(STATS_BASE_URL + "/teams/" + strconv.Itoa(id) + "/stats")
@@ -133,4 +143,81 @@ type Stat struct {
 			Link string `json:"link"`
 		} `json:"team"`
 	} `json:"splits"`
+}
+
+type TeamPlayers struct {
+	Data []struct {
+		ID                      int         `json:"id"`
+		AccruedSeasons          int         `json:"accruedSeasons"`
+		AddNames                string      `json:"addNames"`
+		AgeSignWaiver           int         `json:"ageSignWaiver"`
+		AgeSignelFa             int         `json:"ageSignelFa"`
+		Alert                   string      `json:"alert"`
+		BirthCity               string      `json:"birthCity"`
+		BirthCountry            string      `json:"birthCountry"`
+		BirthDate               string      `json:"birthDate"`
+		BirthStateProvince      interface{} `json:"birthStateProvince"`
+		CareerTeamID            interface{} `json:"careerTeamId"`
+		CentralRegistryPosition string      `json:"centralRegistryPosition"`
+		ClubElecArb             string      `json:"clubElecArb"`
+		CurrentTeamID           int         `json:"currentTeamId"`
+		DateOfDeath             interface{} `json:"dateOfDeath"`
+		DdaID                   int         `json:"ddaId"`
+		Deceased                bool        `json:"deceased"`
+		EpPlayerID              int         `json:"epPlayerId"`
+		FaGroupAfterSeason      interface{} `json:"faGroupAfterSeason"`
+		FirstName               string      `json:"firstName"`
+		FirstSignedByTeamID     interface{} `json:"firstSignedByTeamId"`
+		FreeAgentGroup          interface{} `json:"freeAgentGroup"`
+		FullName                string      `json:"fullName"`
+		Group5Election          string      `json:"group5Election"`
+		Group5SeasonsEarned     int         `json:"group5SeasonsEarned"`
+		Group6Proration         interface{} `json:"group6Proration"`
+		Group6SeasonsEarned     int         `json:"group6SeasonsEarned"`
+		GroupsEarnedThruSeason  int         `json:"groupsEarnedThruSeason"`
+		Height                  int         `json:"height"`
+		HofInductionYear        interface{} `json:"hofInductionYear"`
+		HomeTown                interface{} `json:"homeTown"`
+		IihfHofInductionYear    interface{} `json:"iihfHofInductionYear"`
+		InHockeyHof             bool        `json:"inHockeyHof"`
+		InIihfHof               int         `json:"inIihfHof"`
+		InTop100Alltime         bool        `json:"inTop100Alltime"`
+		InUsHockeyHof           bool        `json:"inUsHockeyHof"`
+		IsDefected              string      `json:"isDefected"`
+		IsDeleted               string      `json:"isDeleted"`
+		IsJunior                string      `json:"isJunior"`
+		IsRetired               interface{} `json:"isRetired"`
+		IsRookie                string      `json:"isRookie"`
+		IsSuspended             string      `json:"isSuspended"`
+		LastAmateurLeagueID     int         `json:"lastAmateurLeagueId"`
+		LastAmateurTeamID       int         `json:"lastAmateurTeamId"`
+		LastNHLTeamID           int         `json:"lastNHLTeamId"`
+		LastName                string      `json:"lastName"`
+		LoanCapException        string      `json:"loanCapException"`
+		LongTermInjury          string      `json:"longTermInjury"`
+		Message                 interface{} `json:"message"`
+		MiddleName              interface{} `json:"middleName"`
+		Nationality             string      `json:"nationality"`
+		NhlExperience           int         `json:"nhlExperience"`
+		OnRoster                string      `json:"onRoster"`
+		PlatformYear            int         `json:"platformYear"`
+		Position                string      `json:"position"`
+		PrName                  string      `json:"prName"`
+		PrStat                  int         `json:"prStat"`
+		ProYearReduction        interface{} `json:"proYearReduction"`
+		ReentryWaivers          string      `json:"reentryWaivers"`
+		RosterSpecialCode       interface{} `json:"rosterSpecialCode"`
+		SalaryArbitrationExp    int         `json:"salaryArbitrationExp"`
+		ShootsCatches           string      `json:"shootsCatches"`
+		SweaterNumber           int         `json:"sweaterNumber"`
+		UpdateTimestamp         string      `json:"updateTimestamp"`
+		UsHofInductionYear      interface{} `json:"usHofInductionYear"`
+		VetCapExcptn            string      `json:"vetCapExcptn"`
+		WaiverAmount            int         `json:"waiverAmount"`
+		WaiverDraft             string      `json:"waiverDraft"`
+		WaiverStatus            string      `json:"waiverStatus"`
+		Weight                  int         `json:"weight"`
+		YearsPro                int         `json:"yearsPro"`
+	} `json:"data"`
+	Total int `json:"total"`
 }
